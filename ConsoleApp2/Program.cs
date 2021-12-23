fe4b081267e61e9cae7728c4e80a1f3bd59f1dd9
Gvantsa19 <oqropiridzegvantsa@gmail.com>
2021-12-23 21:52:59 +0400

    create dice game

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int playerNumber;
+            int enemyNumber;
+
+            int playerPoint = 0;
+            int enemyPoint = 0; 
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("please press any key to roll the dice");
+                Console.WriteLine("");
+                Console.ReadKey();
+                playerNumber = random.Next(1, 7);
+                Console.WriteLine("player rolled: " + playerNumber);
+
+                Console.WriteLine(".......");
+                System.Threading.Thread.Sleep(700);
+
+                enemyNumber = random.Next(1, 7);
+                Console.WriteLine("enemy rolled: " + enemyNumber);
+                Console.WriteLine("");
+
+                if (playerNumber > enemyNumber)
+                {
+                    playerPoint++;
+                    Console.WriteLine("player won this round!");
+                    Console.WriteLine("");
+                }
+                else if (playerNumber < enemyNumber)
+                {
+                    enemyPoint++;
+                    Console.WriteLine("enemy won this round!");
+                    Console.WriteLine("");
+                }
+                else
+                {
+                    Console.WriteLine("it is a draw!");
+                    Console.WriteLine("");
+                }
+            }
+
+            if (playerPoint > enemyPoint)
+            {
+                Console.WriteLine("player won this game!");
+            }
+            else if (playerPoint < enemyPoint)
+            {
+                Console.WriteLine("enemy won this game!");
+
+            }
+            else
+            {
+                Console.WriteLine("it is a draw!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
